b295a6b46b70af42cedc2c0430d5cf52b957a41b
Martin Marosi <marvusm.mmi@gmail.com>
2019-06-25 08:51:11 +0200

    DO NOT MERGE

---
+++ packages/pf3-component-mapper/src/tests/output-poc.test.js
@@ -0,0 +1,16 @@
+describe('output POC', () => {
+  const createString = (length) => {
+    let result           = '';
+    let characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    let charactersLength = characters.length;
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+
+    return result;
+  };
+
+  it('shout console log snapshot tree', () => {
+    console.log(createString(5000000));
+  });
+});
