379f6287daa7a57d31d3de0918a38fb502ab971a
Martin Marosi <marvusm.mmi@gmail.com>
2019-07-30 15:31:40 +0200

    fix(pf4): migrate to css for select styles.

--- packages/pf4-component-mapper/src/form-fields/select/select.js
+++ packages/pf4-component-mapper/src/form-fields/select/select.js
@@ -1,116 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-import { Button } from '@patternfly/react-core';
-import ReactSelect, { components } from 'react-select';
+import ReactSelect from 'react-select';
 import CreatableSelect from 'react-select/creatable';
-import { TimesCircleIcon, CaretDownIcon, CheckIcon } from '@patternfly/react-icons';
 
-import selectStyles from './select-styles';
+import MultiValueContainer from './multi-value-container';
+import ValueContainer from './value-container';
+import MultiValueRemove from './multi-value-remove';
+import DropdownIndicator from './dropdown-indicator';
+import ClearIndicator from './clear-indicator';
+import Option from './option';
+
 import './select-styles.scss';
-
-const MultiValueContainer = (props, ...rest) => (
-  <div className="pf4-ddforms-multiselect-chip" title={ props.data.label }>
-    <span>{ props.children[0] }</span>
-    <span className="multi-value-remove-button">{ props.children[1] }</span>
-  </div>
-);
-
-MultiValueContainer.propTypes = {
-  children: PropTypes.oneOfType([ PropTypes.node, PropTypes.arrayOf(PropTypes.node) ]).isRequired,
-  data: PropTypes.shape({ label: PropTypes.string.isRequired }).isRequired,
-};
-
-const MultiValueRemove = props => (
-  <components.MultiValueRemove { ...props }>
-    <TimesCircleIcon style={{ fontSize: 12 }} />
-  </components.MultiValueRemove>
-);
-
-const CHIP_CLEAR_BACKGROUND_COLOR = '#72767b';
-const CHIP_CLEAR_BACKGROUND_HOVER_COLOR = '#151515';
-
-class ClearIndicator extends Component {
-  state = {
-    fill: CHIP_CLEAR_BACKGROUND_COLOR,
-  }
-  render() {
-    const { innerProps: { ref, ...restInnerProps }} = this.props;
-    const { fill } = this.state;
-    return (
-      <TimesCircleIcon
-        { ...restInnerProps }
-        onClick={ this.props.clearValue }
-        onMouseEnter={ () => this.setState(() => ({ fill: CHIP_CLEAR_BACKGROUND_HOVER_COLOR })) }
-        onMouseLeave={ () => this.setState(() => ({ fill: CHIP_CLEAR_BACKGROUND_COLOR })) }
-        fill={ fill }
-      />
-    );
-  }
-}
-
-ClearIndicator.propTypes = {
-  innerProps: PropTypes.object.isRequired,
-  clearValue: PropTypes.func.isRequired,
-  getStyles: PropTypes.func.isRequired,
-};
-
-class ValueContainer extends Component {
-  state = {
-    showAll: false,
-  }
-  render () {
-    const { isMulti, ...props } = this.props;
-    const { showAll } = this.state;
-    if (isMulti) {
-      return (
-        <div className="pf4-ddforms-multi-value">
-          { showAll ? props.children[0] : props.children[0] && props.children[0][0] ? props.children[0][0] : props.children[0] }
-          { props.children[0] && props.children[0].length > 1 && (
-            <Button
-              className="pf4-ddforms-multiselect-chipgroup-button"
-              onClick={ () => this.setState(({ showAll }) => ({ showAll: !showAll })) }
-              variant="plain"
-            >
-              <span>{ showAll ? props.selectProps.showLessLabel : `${ props.children[0].length - 1 } ${props.selectProps.showMoreLabel}` }</span>
-            </Button>
-          ) }
-          { props.children[1] && props.children[1] }
-        </div>
-      );
-    }
-
-    return props.children;
-  }
-}
-
-ValueContainer.propTypes = {
-  isMulti: PropTypes.bool.isRequired,
-  children: PropTypes.oneOfType([ PropTypes.node, PropTypes.arrayOf(PropTypes.node) ]).isRequired,
-};
-
-const DropdownIndicator = () => {
-  return <CaretDownIcon style={{ marginLeft: 8, marginRight: 8 }} />;
-};
-
-const Option = props => {
-  return (
-    <div
-      className={ `pf4-ddforms-select-menu-option ${
-        props.isFocused ? 'focused' : ''
-      }` }
-    >
-      <components.Option { ...props } />
-      { props.isSelected && (
-        <CheckIcon size="sm" fill="#06c" style={{ paddingRight: 8 }} />
-      ) }
-    </div>
-  );
-};
-
-Option.propTypes = {
-  isFocused: PropTypes.bool.isRequired,
-  isSelected: PropTypes.bool.isRequired,
-};
 
 const selectProvider = type => ({
   default: ReactSelect,
@@ -148,7 +48,8 @@ export class Select extends React.Component {
 
     if (isLoading){
       return (<Select
-        styles={ selectStyles }
+        className="ddorg__pf4-component-mapper__select"
+        classNamePrefix="ddorg__pf4-component-mapper__select"
         isDisabled={ true }
         placeholder={ loadingMessage }
       />);
@@ -156,7 +57,6 @@ export class Select extends React.Component {
 
     return (
       <Select
-        styles={ selectStyles }
         menuPlacement="auto"
         components={{
           MultiValueContainer,
@@ -167,6 +67,8 @@ export class Select extends React.Component {
           Option,
         }}
         { ...props }
+        className="ddorg__pf4-component-mapper__select"
+        classNamePrefix="ddorg__pf4-component-mapper__select"
         onChange={ (option) => {
           const o =  !option && props.isMulti ? [] : option;
           return simpleValue
---
+++ packages/pf4-component-mapper/src/form-fields/select/value-container.js
@@ -0,0 +1,56 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Button } from '@patternfly/react-core';
+
+class ValueContainer extends Component {
+    state = {
+      showAll: false,
+    }
+    render() {
+      const { isMulti, ...props } = this.props;
+      const { showAll } = this.state;
+      if (isMulti && props.children) {
+        return (
+          <div className="ddorg__pf4-component-mapper__select__value--container">
+            { showAll ?
+              props.children[0] :
+              props.children[0] && props.children[0][0] ? props.children[0][0] : props.children[0] }
+            { props.children[0] && props.children[0].length > 1 && (
+              <Button
+                className="ddorg__pf4-component-mapper__select__value--container-chipgroup"
+                onClick={ () => this.setState(({ showAll }) => ({ showAll: !showAll })) }
+                variant="plain"
+              >
+                <span>{ showAll ?
+                  props.selectProps.showLessLabel :
+                  `${props.children[0].length - 1} ${props.selectProps.showMoreLabel}` }</span>
+              </Button>
+            ) }
+            { Array.isArray(props.children) ? props.children[1] && props.children[1] : props.children }
+          </div>
+        );
+      }
+
+      return props.children;
+    }
+}
+
+ValueContainer.propTypes = {
+  isMulti: PropTypes.bool,
+  getStyles: PropTypes.func.isRequired,
+  children: PropTypes.oneOfType([ PropTypes.node, PropTypes.arrayOf(PropTypes.node) ]).isRequired,
+  selectProps: PropTypes.shape({
+    showLessLabel: PropTypes.string,
+    showMoreLabel: PropTypes.string,
+  }),
+};
+
+ValueContainer.defaultProps = {
+  isMulti: false,
+  selectProps: {
+    showLessLabel: 'Show less',
+    showMoreLabel: 'more',
+  },
+};
+
+export default ValueContainer;
