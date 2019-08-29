46807f2853434454b14bc88ffe87ca01674787dd
Martin Marosi <marvusm.mmi@gmail.com>
2019-08-29 12:59:56 +0200

    Added layout to component API doc

--- packages/react-renderer-demo/src/common/doc-texts/component-api.js
+++ packages/react-renderer-demo/src/common/doc-texts/component-api.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import makeStyles from '@material-ui/styles/makeStyles';
 
 import MdxContent from 'docs/components/component-api/component-api.md';
 import ListOfContent from '../helpers/list-of-content';
@@ -9,10 +10,37 @@ const reqSource = require.context(
   /\.md/,
 );
 
-export default (
-  <React.Fragment>
-    <ListOfContent text={ reqSource('./component-api/component-api.md').default } />
-    <MdxContent />
-  </React.Fragment>
-);
+const useStyles = makeStyles(() => ({
+  demoWrapper: {
+    displa: 'flex',
+  },
+  mdxWrapper: {
+    width: 'calc(100% - 240px)',
+  },
+  contentWrapper: {
+    width: 240,
+    position: 'fixed',
+    right: 0,
+    top: 96,
+    overflow: 'auto',
+    height: 'calc(100vh - 96px)',
+  },
+}));
+
+const ComponentApi = () => {
+
+  const classes = useStyles();
+  return (
+    <div className={ classes.demoWrapper }>
+      <div className={ classes.mdxWrapper }>
+        <MdxContent />
+      </div>
+      <div className={ classes.contentWrapper }>
+        <ListOfContent text={ reqSource('./component-api/component-api.md').default } />
+      </div>
+    </div>
+  );
+};
+
+export default (<ComponentApi />);
 
