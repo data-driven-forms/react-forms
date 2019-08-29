264f04ae4e9599c29790d392e66a6ecde62db84d
Martin Marosi <marvusm.mmi@gmail.com>
2019-08-29 14:42:20 +0200

    Removed react-markdown library.

--- packages/react-renderer-demo/src/common/other-text/manageiq-components.js
+++ packages/react-renderer-demo/src/common/other-text/manageiq-components.js
@@ -1,99 +1,46 @@
 import React from 'react';
-import ReactMarkdown from '../md-helper';
-import TableOfContent from '../helpers/list-of-content';
+import makeStyles from '@material-ui/styles/makeStyles';
 
-const text =  `
-### Duallist select
+import MdxContent from 'docs/components/manageiq-components.md';
+import ListOfContent from '../helpers/list-of-content';
 
-**Props**
+const reqSource = require.context(
+  '!raw-loader!docs/components/',
+  true,
+  /\.md/,
+);
 
-| Props  | Type | Default |  Decription |
-| ------------- | ------------- | ------------- | ------------- |
-| leftId  | string  | undefined  | ID passed to left select  |
-| rightId  | string  | undefined  | ID passed to right select  |
-| leftTitle  | string  | undefined  | Title above left select  |
-| rightTitle  | string  | undefined  | Title above right select  |
-| rightTitle  | string  | undefined  | Title above right select  |
-| size  | number  | 15  | Size of the selects (in lines)  |
-| allToRight  | bool  | true  | Should show moveAllToRight button  |
-| allToLeft | bool  | false  | Should show moveAllToLeft button  |
-| moveLeftTitle  | string  | __('Move selected to left')  | Title of moveToLeft button |
-| moveRightTitle  | string  | __('Move selected to right')  | Title of moveToRight button |
-| moveAllRightTitle  | string  | __('Move all to right')  | Title of moveAllToRight button |
-| moveAllLeftTitle  | string  | __('Move all to left')  | Title of moveAllToLeft button |
-| options  | array | [ ]  | All options of the select |
-| input: { value }  | array | [ ]  | Selected options **(subset of options)** |
+const useStyles = makeStyles(() => ({
+  demoWrapper: {
+    displa: 'flex',
+  },
+  mdxWrapper: {
+    width: 'calc(100% - 240px)',
+  },
+  contentWrapper: {
+    width: 240,
+    position: 'fixed',
+    right: 0,
+    top: 96,
+    overflow: 'auto',
+    height: 'calc(100vh - 96px)',
+  },
+}));
 
-**Options/Value format**
+const ManageIQComponents = () => {
 
-\`\`\`jsx
-[
-      {key: 'key', label: 'label'},
-      {key: 'key1', label: 'label1'},
-      {key: 'key2', label: 'label2'},
-      ...
-]
-\`\`\`
+  const classes = useStyles();
+  return (
+    <div className={ classes.demoWrapper }>
+      <div className={ classes.mdxWrapper }>
+        <MdxContent />
+      </div>
+      <div className={ classes.contentWrapper }>
+        <ListOfContent text={ reqSource('./manageiq-components.md').default } />
+      </div>
+    </div>
+  );
+};
 
-The component will split values from options to left/right select lists according to value. (All values in value are going to right, others to left)
+export default (<ManageIQComponents />);
 
-**How to use it in Data-driven form schema (example)**
-
-\`\`\`jsx
-{   
-    component: 'dual-list-select',
-    name: 'duallist',
-    options: [
-      { key: 'key', label: 'label' }, ...
-    ],
-    rightId: 'child_vms',
-    leftId: 'available_vms',
-    rightTitle: __('Child VMs:'),
-    leftTitle: __('Availables VMs'),
-    moveLeftTitle: __('Move selected VMs to left'),
-    moveRightTitle: __('Move selected VMs to right'),
-    moveAllRightTitle: __('Move all VMs to right'),
-}
-\`\`\`
-
-**How to use it in Data-driven form**
-
-By default, the value contains all options from the right side select. However, if you need left values or just added values (probably in most cases), you can use helpers in \`/dual-list-select/helpers\` to extract needed information:
-
-<br />
-
-**1** In constructor/componentDidMount/elsewhere (where you fetch data) save these values into state:
-\`originalOptions: options\`
-\`originalRightValues: value\`
-
-<br />
-
-**2** In your submit method (values => (...)) you can use helper methods:
-
-<br />
-
-**\`filterOptions(originalOptions, values.duallist)\`** to get all values on left \n
-**\`filterOptions(values.duallist, originalOptions)\`** to get added values to left select \n
-**\`filterOptions(values.duallist, originalRightValues)\`** to get added values to right select \n
-**\`getKeys(values)\`** to get keys of values \n
-
-<br />
-
-**3** Then you can send values to endpoints/API!
-
-### Horizontal rule
-
-Just a \`hr\` component for using in forms.
-
-\`\`\`jsx
-{   
-    component: 'hr',
-    name: 'name', // every component has to have an unique name!
-}
-\`\`\`
-`;
-
-export default <React.Fragment>
-  <TableOfContent text={ text } />
-  <ReactMarkdown source={ text } />
-</React.Fragment>;
