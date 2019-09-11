d603c34c4dc5434d12309b93695cf8caf7f6d824
Martin Marosi <marvusm.mmi@gmail.com>
2019-09-11 17:54:09 +0200

    Fixed wrong example at landing page.

--- packages/react-renderer-demo/src/components/landing-page-cards.js
+++ packages/react-renderer-demo/src/components/landing-page-cards.js
@@ -11,14 +11,17 @@ const value = `import React from 'react';
 import FormRenderer, { componentTypes } from '@data-driven-forms/react-form-renderer';
 import { formFieldsMapper, layoutMapper } from '@data-driven-forms/pf4-component-mapper';
 
-const schema = [{
-  component: componentTypes.TEXT_FIELD,
-  name: 'name',
-  label: 'Your name'
-}]
+const schema = {
+  fields: [{
+    component: componentTypes.TEXT_FIELD,
+    name: 'name',
+    label: 'Your name'
+  }]
+}
 
 const Form = () => (
   <FormRenderer
+    schema={schema}
     formFieldsMapper={formFieldsMapper}
     layoutMapper={layoutMapper}
     onSubmit={console.log}
