3672569cbbd9ff97b6652fbaf78414cc15c0ed34
Martin Marosi <marvusm.mmi@gmail.com>
2019-09-16 15:36:35 +0200

    Added basic navigation

--- packages/react-renderer-demo/pages/_app.js
+++ packages/react-renderer-demo/pages/_app.js
@@ -20,7 +20,7 @@ export default class MyApp extends App {
     return (
       <React.Fragment>
         <Head>
-          <title>My page</title>
+          <title>Data driven forms</title>
         </Head>
         <ThemeProvider theme={ theme }>
           { /* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */ }
