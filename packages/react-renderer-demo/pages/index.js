702eebb0ce2d5b0df8a4f081b09da917a7311af2
Martin Marosi <marvusm.mmi@gmail.com>
2019-09-16 15:40:14 +0200

    Moved layout to common component

--- packages/react-renderer-demo/pages/index.js
+++ packages/react-renderer-demo/pages/index.js
@@ -7,14 +7,8 @@ import Layout from '../src/components/layout';
 
 export default function Index() {
   return (
-    <Layout>
-      <Container maxWidth="sm">
-        <Box my={ 4 }>
-          <Typography variant="h4" component="h1" gutterBottom>
-            Next.js example
-          </Typography>
-        </Box>
-      </Container>
-    </Layout>
+    <h1>
+      There wil be dragons
+    </h1>
   );
 }
