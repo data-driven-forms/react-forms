48b7052b50484c0ecff55f8a1dcefe27ca013056
Martin Marosi <marvusm.mmi@gmail.com>
2019-09-17 12:45:20 +0200

    Added pf css file.

--- packages/react-renderer-demo/pages/_document.js
+++ packages/react-renderer-demo/pages/_document.js
@@ -19,6 +19,7 @@ class MyDocument extends Document {
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
           />
+          <link href="https://fonts.googleapis.com/css?family=Montserrat:700&display=swap" rel="stylesheet"/>
         </Head>
         <body>
           <Main />
