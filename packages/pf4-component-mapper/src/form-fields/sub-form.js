afc6c930b6ef17899917625a9eaf9cfc9c559a55
Richard Vsiansky <r.vsia@seznam.cz>
2019-10-11 12:03:33 +0200

    fix(subform): allow to receive other props

--- packages/pf4-component-mapper/src/form-fields/sub-form.js
+++ packages/pf4-component-mapper/src/form-fields/sub-form.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Title } from '@patternfly/react-core';
-import { TextContent, Text, TextVariants } from '@patternfly/react-core';
+import { TextContent, Text, TextVariants, Grid, GridItem } from '@patternfly/react-core';
 
-const SubForm = ({ fields, title, description, formOptions }) => (
-  <React.Fragment>
-    { title && <Title size="xl">{ title }</Title> }
-    { description && <TextContent><Text component={ TextVariants.small } style={{ marginBottom: 0 }}>{ description }</Text></TextContent> }
+const SubForm = ({
+  fields,
+  title,
+  description,
+  formOptions,
+  FieldProvider: _FieldProvider,
+  validate: _validate,
+  ...rest
+}) => (
+  <Grid gutter="md" { ...rest }>
+    { title && <GridItem sm={ 12 }><Title size="xl">{ title }</Title></GridItem> }
+    { description && <GridItem sm={ 12 }>
+      <TextContent>
+        <Text component={ TextVariants.small } style={{ marginBottom: 0 }}>{ description }
+        </Text>
+      </TextContent>
+    </GridItem> }
     { formOptions.renderForm(fields, formOptions) }
-  </React.Fragment>
+  </Grid>
 );
 
 SubForm.propTypes = {
@@ -19,6 +32,8 @@ SubForm.propTypes = {
   name: PropTypes.string,
   title: PropTypes.string,
   description: PropTypes.string,
+  FieldProvider: PropTypes.any,
+  validate: PropTypes.any,
 };
 
 export default SubForm;
