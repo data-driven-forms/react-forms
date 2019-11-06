6150c5a00020900252354f299e41c484a564d1e8
Richard Vsiansky <r.vsia@seznam.cz>
2019-11-06 13:54:50 +0100

    Update select demos

---
+++ packages/react-renderer-demo/src/app/src/components/navigation/examples-texts/select.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import GenericComponentText from './generic-mui-component';
+import Pf3Select from '@docs/doc-components/pf3-select.md';
+import Pf4Select from '@docs/doc-components/pf4-select.md';
+
+export default ({ activeMapper }) => activeMapper === 'pf3' ? <Pf3Select /> : activeMapper === 'pf4' ? <Pf4Select /> : <GenericComponentText />;
