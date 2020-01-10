9bc55d59bb968b83574ab27c17ec0d1bc72ffde4
Richard Vsiansky <r.vsia@seznam.cz>
2020-01-10 14:51:50 +0100

    Add prepareMsg tests

--- packages/react-form-renderer/src/tests/validators/helper.test.js
+++ packages/react-form-renderer/src/tests/validators/helper.test.js
@@ -1,4 +1,5 @@
-import { memoize } from '../../validators/helpers';
+import React from 'react';
+import { memoize, prepareMsg } from '../../validators/helpers';
 
 describe('Validators helpers', () => {
   describe('memoize', () => {
@@ -48,6 +49,22 @@ describe('Validators helpers', () => {
       expect(fn.mock.calls.length).toBe(2);
       fakeValidator([ 'a', 'b' ]);
       expect(fn.mock.calls.length).toBe(2);
+    });
+  });
+
+  describe('prepareMsg', () => {
+    const values = { x: '786727' };
+
+    it('returns element messages', () => {
+      expect(prepareMsg(<div type='prop type' />, 'type', values)).toEqual(
+        { defaultMessage: 'prop type', id: 'prop type', values }
+      );
+    });
+
+    it('returns object messages', () => {
+      expect(prepareMsg({ id: 'i3662', defaultMessage: 'message' }, 'type', values)).toEqual(
+        { defaultMessage: 'message', id: 'i3662', values }
+      );
     });
   });
 });
