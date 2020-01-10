4543aa535a0e11e240fd37af391a2a436c538c9b
Richard Vsiansky <r.vsia@seznam.cz>
2020-01-10 15:24:31 +0100

    Add schemaErrorComponent test

---
+++ packages/react-form-renderer/src/tests/form-renderer/schema-error-component.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import SchemaErrorComponent from '../../form-renderer/schema-error-component';
+
+describe('schemaErrorComponent', () => {
+  it('renders correctly', () => {
+    const message = 'Error message';
+    const name = 'Invalid schema :(';
+
+    const wrapper = mount(<SchemaErrorComponent message={ message } name={ name }/>);
+
+    expect(wrapper.find('h1')).toHaveLength(1);
+
+    expect(wrapper.find('h2')).toHaveLength(1);
+    expect(wrapper.find('h2').text().includes(name)).toEqual(true);
+
+    expect(wrapper.find('p')).toHaveLength(2);
+    expect(wrapper.find('p').first().text().includes(message)).toEqual(true);
+  });
+});
