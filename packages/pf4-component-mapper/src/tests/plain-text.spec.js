cdd8f168851097594e8b00a8c5051a632c5a8ccc
Richard Vsiansky <r.vsia@seznam.cz>
2020-01-10 13:04:10 +0100

    Add tests for PF4 plain text

---
+++ packages/pf4-component-mapper/src/tests/plain-text.spec.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { mount } from 'enzyme';
+import { TextContent, Text } from '@patternfly/react-core';
+
+import PlainText from '../form-fields/plain-text';
+
+describe('PlainText component', () => {
+  it('renders correctly with three paragraphs', () => {
+    const label = 'One \n Two \n Three';
+    const name = 'name';
+
+    const wrapper = mount(<PlainText name={ name } label={ label }/>);
+
+    expect(wrapper.find(TextContent)).toHaveLength(1);
+    expect(wrapper.find(Text)).toHaveLength(3);
+  });
+
+  it('renders correctly with one paragraphs', () => {
+    const label = 'One';
+    const name = 'name';
+
+    const wrapper = mount(<PlainText name={ name } label={ label }/>);
+
+    expect(wrapper.find(TextContent)).toHaveLength(1);
+    expect(wrapper.find(Text)).toHaveLength(1);
+  });
+});
