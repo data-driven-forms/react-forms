f4e10fa6f5b7088acf030f0ad88a034dff8f34f3
Richard Vsiansky <r.vsia@seznam.cz>
2020-01-14 15:44:03 +0100

    Convert PF4 multiple choice list to use common

--- packages/pf4-component-mapper/src/form-fields/multiple-choice-list.js
+++ packages/pf4-component-mapper/src/form-fields/multiple-choice-list.js
@@ -1,64 +1,39 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Checkbox, FormGroup } from '@patternfly/react-core';
-import { composeValidators } from '@data-driven-forms/react-form-renderer';
+import MultipleChoiceListCommon, { wrapperProps } from '@data-driven-forms/common/src/multiple-choice-list';
 
-const propTypes = {
-  validate: PropTypes.oneOfType([ PropTypes.array, PropTypes.func ]),
-  FieldProvider: PropTypes.oneOfType([ PropTypes.node, PropTypes.func ]),
-  name: PropTypes.string.isRequired,
-};
-
-const MultipleChoiceList = ({ validate, FieldProvider, ...props }) => (
-  <FieldProvider
-    { ...props }
-    validate={ composeValidators(props.validate || []) }
-    render={ ({
-      label,
-      isRequired,
-      helperText,
-      meta,
-      options,
-      isDisabled,
-      isReadOnly,
-      ...rest
-    }) => {
-      const { error, touched } = meta;
-      const showError = touched && error;
-      const groupValues = rest.input.value;
-      return (
-        <FormGroup label={ label } fieldId={ rest.id || rest.key || rest.name } isValid={ showError } >
-          { options.map(option =>
-            (<FieldProvider
-              formOptions={ rest.formOptions }
-              id={ `${rest.id}-${option.value}` }
-              key={ option.value }
-              { ...option }
-              name={ props.name }
-              type="checkbox"
-              render={ ({ input, meta, formOptions, componentType, ...rest }) => {
-                const indexValue = groupValues.indexOf(input.value);
-                return (
-                  <Checkbox
-                    label={ rest.label }
-                    aria-label={ option['aria-label'] || option.label }
-                    isChecked={ input.checked }
-                    { ...input }
-                    { ...rest }
-                    isDisabled={ isDisabled || isReadOnly }
-                    onChange={ () => (indexValue === -1
-                      ? input.onChange([ ...groupValues, input.value ])
-                      : input.onChange([ ...groupValues.slice(0, indexValue), ...groupValues.slice(indexValue + 1) ])) }
-                  />
-                );
-              } }
-            />)) }
-        </FormGroup>
-      );
-    } }
-  />
+const FinalCheckbox = (props) => (
+  <Checkbox isChecked={ props.checked } { ...props }/>
 );
 
-MultipleChoiceList.propTypes = propTypes;
+FinalCheckbox.propTypes = {
+  checked: PropTypes.bool,
+};
+
+const Wrapper = ({ showError, isRequired, helperText, label, meta, children, rest }) =>(
+  <FormGroup
+    label={ label }
+    fieldId={ rest.id || rest.key || rest.name }
+    isValid={ !showError }
+    isRequired={ isRequired }
+    helperText={ helperText }
+    helperTextInvalid={ meta.error }
+  >
+    { children }
+  </FormGroup>
+);
+
+Wrapper.propTypes = {
+  ...wrapperProps,
+};
+
+const MultipleChoiceList = (props) => (
+  <MultipleChoiceListCommon
+    { ...props }
+    Wrapper={ Wrapper }
+    Checkbox={ FinalCheckbox }
+  />
+);
 
 export default MultipleChoiceList;
