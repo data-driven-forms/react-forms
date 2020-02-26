59b39e14e026c76e4ac9bd2d4af7827e24de22b5
Martin Maroši <marvusm.mmi@gmail.com>
2020-02-26 08:53:51 +0100

    Removed unused prop types from field provider.

--- packages/react-form-renderer/src/components/field-provider.js
+++ packages/react-form-renderer/src/components/field-provider.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import dataTypes from './data-types';
 import useFieldApi from '../hooks/use-field-api';
 
 const FieldProvider = ({ component, render, ...props }) => {
@@ -19,25 +18,8 @@ const FieldProvider = ({ component, render, ...props }) => {
 };
 
 FieldProvider.propTypes = {
-  formOptions: PropTypes.shape({
-    clearOnUnmount: PropTypes.bool,
-    change: PropTypes.func,
-    getFieldState: PropTypes.func,
-    clearedValue: PropTypes.any
-  }),
-  clearedValue: PropTypes.any,
   component: PropTypes.oneOfType([PropTypes.node, PropTypes.element, PropTypes.func]),
-  render: PropTypes.func,
-  children: PropTypes.oneOfType([PropTypes.node, PropTypes.element, PropTypes.func]),
-  dataType: PropTypes.oneOf(Object.values(dataTypes)),
-  name: PropTypes.string,
-  clearOnUnmount: PropTypes.bool,
-  initializeOnMount: PropTypes.bool,
-  initialValue: PropTypes.any
-};
-
-FieldProvider.defaultProps = {
-  formOptions: {}
+  render: PropTypes.func
 };
 
 export default FieldProvider;
