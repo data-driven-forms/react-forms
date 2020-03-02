94d1bca231b4909e1d404fe1072f51b488df99ac
Martin Maroši <marvusm.mmi@gmail.com>
2020-03-02 14:22:06 +0100

    Update react dependency version to 16.13.0

--- packages/pf3-component-mapper/src/components/button.js
+++ packages/pf3-component-mapper/src/components/button.js
@@ -2,8 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Button as PFButton } from 'patternfly-react';
 
-export const Button = ({ label, variant, className, ...rest }) => (
-  <PFButton bsStyle={variant} className={className} {...rest}>
+export const Button = ({ label, variant, dataType, validate, ...rest }) => (
+  <PFButton bsStyle={variant} {...rest}>
     {label}
   </PFButton>
 );
@@ -11,11 +11,9 @@ export const Button = ({ label, variant, className, ...rest }) => (
 Button.propTypes = {
   label: PropTypes.string.isRequired,
   variant: PropTypes.string,
-  className: PropTypes.string
-};
-
-Button.defaultProps = {
-  className: ''
+  className: PropTypes.string,
+  dataType: PropTypes.any, // should be inside inner props or something
+  validate: PropTypes.any // should be inside inner props or something
 };
 
 export default Button;
