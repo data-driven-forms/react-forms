a244299cfcc36d4bb5166c4f453c765352a05db3
Martin Maroši <marvusm.mmi@gmail.com>
2020-03-04 12:38:59 +0100

    Rewrite mappers FormTemplates to be components and remove onState update from them.

--- packages/react-form-renderer/src/tests/form-renderer/render-form.test.js
+++ packages/react-form-renderer/src/tests/form-renderer/render-form.test.js
@@ -1185,7 +1185,7 @@ describe('renderForm function', () => {
     });
   });
 
-  it('should use actionMapper', () => {
+  it.only('should use actionMapper', () => {
     const id = '23asd86';
 
     const intl = jest.fn().mockImplementation((id) => `translated ${id}`);
