46cf8a1bf9400c32bf4fe65d81462581fa47d64b
T Arun <arunthulasiraman@gmail.com>
2020-03-20 18:05:07 +0530

    Added text-field.js
    The validation(required) isn't working as intended. Unable to get it validate on submit.

--- packages/ant-component-mapper/src/components/component-mapper.js
+++ packages/ant-component-mapper/src/components/component-mapper.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { componentTypes } from '@data-driven-forms/react-form-renderer';
 import Tabs from './tabs';
 import PlainText from './plain-text';
+import TextField from './text-field';
 
 export const components = {};
 
 const componentMapper = {
-  [componentTypes.TEXT_FIELD]: () => <div>Text field</div>,
+  [componentTypes.TEXT_FIELD]: TextField,
   [componentTypes.TEXTAREA]: () => <div>Textarea field</div>,
   [componentTypes.SELECT]: () => <div>Select field</div>,
   [componentTypes.CHECKBOX]: () => <div>checkbox field</div>,
