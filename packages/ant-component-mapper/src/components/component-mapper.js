1539b0ad06cf14133a40d93ec84d0c6b7d998044
T Arun <arunthulasiraman@gmail.com>
2020-03-21 11:10:35 +0530

    Added text-area.js
    Fixed the validation on TextField. I think the validation works as expected on TextField and TextArea.

--- packages/ant-component-mapper/src/components/component-mapper.js
+++ packages/ant-component-mapper/src/components/component-mapper.js
@@ -3,20 +3,21 @@ import { componentTypes } from '@data-driven-forms/react-form-renderer';
 import Tabs from './tabs';
 import PlainText from './plain-text';
 import TextField from './text-field';
+import TextArea from './text-area';
+//import Checkbox from './checkbox';
 
 export const components = {};
 
 const componentMapper = {
   [componentTypes.TEXT_FIELD]: TextField,
-  [componentTypes.TEXTAREA]: () => <div>Textarea field</div>,
+  [componentTypes.TEXTAREA]: TextArea,
   [componentTypes.SELECT]: () => <div>Select field</div>,
-  [componentTypes.CHECKBOX]: () => <div>checkbox field</div>,
+  [componentTypes.CHECKBOX]: () => <div>Checkbox</div>,
   [componentTypes.SUB_FORM]: () => <div>sub form</div>,
   [componentTypes.RADIO]: () => <div>Radio field</div>,
   [componentTypes.TABS]: Tabs,
   [componentTypes.DATE_PICKER]: () => <div>Date picker</div>,
   [componentTypes.TIME_PICKER]: () => <div>Time picker</div>,
-  [componentTypes.TEXTAREA]: () => <div>Textarea field</div>,
   [componentTypes.SELECT]: () => <div>Select field</div>,
   [componentTypes.PLAIN_TEXT]: PlainText,
   [componentTypes.SWITCH]: <div>Switch</div>
