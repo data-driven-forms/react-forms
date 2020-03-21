831301623c23e5cb6ee2a229ac9b00bf9832f2e9
T Arun <arunthulasiraman@gmail.com>
2020-03-21 11:14:47 +0530

    Added checkbox.js
    Ant does not support required on checkbox. Should look into it and solve the problem. The layout
    is horizontal in multiple checkboxes, can be changed if needed.

---
+++ packages/ant-component-mapper/src/common/multiple-choice-list.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Form, Checkbox as AntCheckbox } from 'antd';
+import MultipleChoiceListCommon, { wrapperProps } from '@data-driven-forms/common/src/multiple-choice-list';
+import { validationError } from './helpers';
+
+const FinalCheckbox = ({ isDisabled, label, ...props }) => (
+  <AntCheckbox {...props} disabled={isDisabled}>
+    {label}
+  </AntCheckbox>
+);
+
+FinalCheckbox.propTypes = {
+  isDisabled: PropTypes.bool,
+  label: PropTypes.node
+};
+
+//not sure if it is necessary to make sure that the checkoboxes are aligned vertiacally when multiple
+const Wrapper = ({ label, isRequired, children, meta, validateOnMount, helperText, description }) => {
+  const invalid = validationError(meta, validateOnMount);
+  return (
+    <Form required={isRequired} layout="vertical" component="fieldset">
+      <Form.Item
+        label={label}
+        help={helperText || description || invalid}
+        validateStatus={!invalid ? '' : 'error'}
+        rules={[
+          {
+            required: isRequired,
+            message: 'Required'
+          }
+        ]} //this is useless
+      >
+        {children}
+      </Form.Item>
+    </Form>
+  );
+};
+
+Wrapper.propTypes = {
+  ...wrapperProps
+};
+
+const MultipleChoiceList = (props) => <MultipleChoiceListCommon {...props} Wrapper={Wrapper} Checkbox={FinalCheckbox} />;
+
+MultipleChoiceList.propTypes = {
+  input: PropTypes.shape({
+    name: PropTypes.string.isRequired
+  })
+};
+
+export default MultipleChoiceList;
