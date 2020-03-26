09a2943f21a32a0e868f435f9f20a9874527e67b
Richard Vsiansky <r.vsia@seznam.cz>
2020-03-26 16:31:17 +0100

    Fix lint in validatorMapper

--- packages/react-form-renderer/src/validators/validator-mapper.js
+++ packages/react-form-renderer/src/validators/validator-mapper.js
@@ -8,8 +8,7 @@ export default {
   [validatorTypes.MIN_LENGTH]: ({ threshold, ...rest }) => length({ minimum: threshold, ...rest }),
   [validatorTypes.MAX_LENGTH]: ({ threshold, ...rest }) => length({ maximum: threshold, ...rest }),
   [validatorTypes.EXACT_LENGTH]: ({ threshold, ...rest }) => length({ is: threshold, ...rest }),
-  [validatorTypes.MIN_ITEMS]: ({ threshold, ...rest }) =>
-    length({ minimum: threshold, message: `Must have at least ${threshold} items.`, ...rest }),
+  [validatorTypes.MIN_ITEMS]: ({ threshold, ...rest }) => length({ minimum: threshold, message: `Must have at least ${threshold} items.`, ...rest }),
   [validatorTypes.PATTERN]: pattern,
   [validatorTypes.MAX_NUMBER_VALUE]: ({ value, includeThreshold = true, ...rest }) =>
     numericality({ [includeThreshold ? '<=' : '<']: value, ...rest }),
