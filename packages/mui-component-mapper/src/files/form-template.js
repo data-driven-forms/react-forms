f8bbd41aa951d618287177eb62d2b9ff4baced95
Richard Vsiansky <r.vsia@seznam.cz>
2020-03-27 09:09:41 +0100

    fix(mui): add proptypes to formtemplate

--- packages/mui-component-mapper/src/files/form-template.js
+++ packages/mui-component-mapper/src/files/form-template.js
@@ -1,5 +1,5 @@
-/* eslint-disable react/prop-types */
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Grid, Button as MUIButton, Typography } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -13,6 +13,11 @@ const useStyles = makeStyles(() => ({
 }));
 
 const Form = ({ children, ...props }) => <form {...props}>{children}</form>;
+
+Form.propTypes = {
+  children: PropTypes.node
+};
+
 const Description = ({ children }) => (
   <Grid item xs={12}>
     <Typography variant="body1" gutterBottom>
@@ -20,6 +25,11 @@ const Description = ({ children }) => (
     </Typography>
   </Grid>
 );
+
+Description.propTypes = {
+  children: PropTypes.node
+};
+
 const Title = ({ children }) => (
   <Grid item xs={12}>
     <Typography variant="h3" gutterBottom>
@@ -27,9 +37,18 @@ const Title = ({ children }) => (
     </Typography>
   </Grid>
 );
+
+Title.propTypes = {
+  children: PropTypes.node
+};
+
 const ButtonGroup = ({ children }) => {
   const classes = useStyles();
   return <div className={classes.buttonGroup}>{children}</div>;
+};
+
+ButtonGroup.propTypes = {
+  children: PropTypes.node
 };
 
 const Button = ({ label, variant, children, buttonType, ...props }) => (
@@ -37,6 +56,13 @@ const Button = ({ label, variant, children, buttonType, ...props }) => (
     {label || children}
   </MUIButton>
 );
+
+Button.propTypes = {
+  children: PropTypes.node,
+  label: PropTypes.node,
+  variant: PropTypes.string,
+  buttonType: PropTypes.string
+};
 
 const MuiFormTemplate = (props) => (
   <FormTemplate FormWrapper={Form} Button={Button} ButtonGroup={ButtonGroup} Title={Title} Description={Description} {...props} />
