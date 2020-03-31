4c16ff89f7076541fd0a1c76c7910193ab82d14a
Martin Maroši <marvusm.mmi@gmail.com>
2020-03-31 15:06:05 +0200

    fix(pf4): fixed radio having bool value instead of input value.

--- packages/pf4-component-mapper/src/files/radio.js
+++ packages/pf4-component-mapper/src/files/radio.js
@@ -4,12 +4,17 @@ import { useFieldApi } from '@data-driven-forms/react-form-renderer';
 import { Radio as Pf4Radio } from '@patternfly/react-core';
 import FormGroup from '../common/form-group';
 
-const RadioOption = ({ name, option, isDisabled, isReadOnly }) => {
-  const { input } = useFieldApi({ name, type: 'radio', value: option.value });
+const RadioOption = ({ name, option, isDisabled, isReadOnly, radioGroupValue }) => {
+  const {
+    input: { checked, ...input }
+  } = useFieldApi({ name, value: option.value });
   return (
     <Pf4Radio
       key={`${name}-${option.value}`}
       {...input}
+      isChecked={radioGroupValue === option.value}
+      value={option.value}
+      onChange={() => input.onChange(option.value)}
       label={option.label}
       id={`${name}-${option.value}`}
       aria-label={option.label}
@@ -22,10 +27,15 @@ RadioOption.propTypes = {
   name: PropTypes.string.isRequired,
   option: PropTypes.shape({ label: PropTypes.node.isRequired, value: PropTypes.any.isRequired }).isRequired,
   isDisabled: PropTypes.bool,
-  isReadOnly: PropTypes.bool
+  isReadOnly: PropTypes.bool,
+  radioGroupValue: PropTypes.any
 };
 
-const Radio = ({ name, options, ...props }) => {
+const Radio = ({ name, options, type, ...props }) => {
+  /**
+   * You cannot assign type radio to PF4 radio buttons input. It will break and will not set input value, only checked property
+   * It has to be reqular input and we have change the radio value manully to the option value
+   */
   const { label, isRequired, helperText, meta, description, hideLabel, input, isReadOnly, isDisabled, id } = useFieldApi({
     name,
     ...props
@@ -41,7 +51,7 @@ const Radio = ({ name, options, ...props }) => {
       id={id || input.name}
     >
       {options.map((option) => (
-        <RadioOption key={option.value} name={name} option={option} isReadOnly={isReadOnly} isDisabled={isDisabled} />
+        <RadioOption radioGroupValue={input.value} key={option.value} name={name} option={option} isReadOnly={isReadOnly} isDisabled={isDisabled} />
       ))}
     </FormGroup>
   );
@@ -57,7 +67,8 @@ Radio.propTypes = {
   isDisabled: PropTypes.bool,
   id: PropTypes.string,
   name: PropTypes.string.isRequired,
-  options: PropTypes.arrayOf(PropTypes.shape({ label: PropTypes.string, value: PropTypes.any })).isRequired
+  options: PropTypes.arrayOf(PropTypes.shape({ label: PropTypes.string, value: PropTypes.any })).isRequired,
+  type: PropTypes.any
 };
 
 export default Radio;
