b57b7f0729209bc1b0bb2f0f0bc9774cb2425482
Richard Vsiansky <r.vsia@seznam.cz>
2020-05-06 09:41:54 +0200

    Do not load PF3 slider on SSR

--- packages/react-renderer-demo/src/app/pages/component-example/slider.js
+++ packages/react-renderer-demo/src/app/pages/component-example/slider.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import dynamic from 'next/dynamic';
 import ComponentText from '@docs/components/component-example-text';
 import useComponentExample from '../../src/hooks/use-component-example';
 import componentTypes from '@data-driven-forms/react-form-renderer/dist/cjs/component-types';
-import PF3Slider from '@data-driven-forms/pf3-component-mapper/dist/cjs/slider';
 import MUISlider from '@data-driven-forms/mui-component-mapper/dist/cjs/slider';
+
+const PF3Slider = dynamic(() => import('@data-driven-forms/pf3-component-mapper/dist/cjs/slider'), {
+  ssr: false
+});
 
 const mappers = {
   pf4: {
