a804f851537f7168a74e22d09560d8c32938d706
Martin Maroši <marvusm.mmi@gmail.com>
2020-05-11 12:59:58 +0200

    feat(suir): cleaned field array from MUI components.

--- packages/suir-component-mapper/src/files/field-array.js
+++ packages/suir-component-mapper/src/files/field-array.js
@@ -2,43 +2,14 @@ import React, { useReducer } from 'react';
 import PropTypes from 'prop-types';
 import { useFormApi, FieldArray } from '@data-driven-forms/react-form-renderer';
 
-import { Grid, Button, Typography, FormControl, FormHelperText, IconButton } from '@material-ui/core';
-
-import { makeStyles } from '@material-ui/core/styles';
-import RedoIcon from '@material-ui/icons/Redo';
-import UndoIcon from '@material-ui/icons/Undo';
+import { Button } from 'semantic-ui-react';
 
 import { useFieldApi } from '@data-driven-forms/react-form-renderer';
 
 import FormFieldGrid from '../common/form-field-grid';
-import clsx from 'clsx';
-
-const useFielArrayStyles = makeStyles({
-  formControl: {
-    width: '100%'
-  },
-  centerText: {
-    display: 'flex',
-    justifyContent: 'center'
-  },
-  buttonsToEnd: {
-    display: 'flex',
-    justifyContent: 'flex-end'
-  },
-  header: {
-    display: 'flex'
-  },
-  label: {
-    flexGrow: 1
-  },
-  fieldArrayGroup: {
-    marginBottom: 32
-  }
-});
 
 const ArrayItem = ({ fields, fieldIndex, name, remove, length, minItems, removeLabel }) => {
   const { renderForm } = useFormApi();
-  const classes = useFielArrayStyles();
 
   const editedFields = fields.map((field, index) => {
     const computedName = field.name ? `${name}.${field.name}` : name;
@@ -46,18 +17,18 @@ const ArrayItem = ({ fields, fieldIndex, name, remove, length, minItems, removeL
   });
 
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <Grid container spacing={3}>
+    <div>
+      <div>
+        <div container spacing={3}>
           {renderForm([editedFields])}
-        </Grid>
-      </Grid>
-      <Grid item xs={12} className={classes.buttonsToEnd}>
+        </div>
+      </div>
+      <div>
         <Button color="secondary" onClick={() => remove(fieldIndex)} disabled={length <= minItems}>
           {removeLabel}
         </Button>
-      </Grid>
-    </Grid>
+      </div>
+    </div>
   );
 };
 
@@ -119,8 +90,6 @@ const DynamicArray = ({ ...props }) => {
     minItems,
     maxItems,
     noItemsMessage,
-    FormFieldGridProps,
-    FormControlProps,
     buttonLabels,
     ...rest
   } = useFieldApi(props);
@@ -131,14 +100,12 @@ const DynamicArray = ({ ...props }) => {
     ...buttonLabels
   };
 
-  const classes = useFielArrayStyles();
-
   const { dirty, submitFailed, error } = meta;
   const isError = (dirty || submitFailed) && error && typeof error === 'string';
 
   return (
-    <FormFieldGrid {...FormFieldGridProps} className={clsx(classes.fieldArrayGroup, FormFieldGridProps.classname)}>
-      <FormControl component="fieldset" error={isError} {...FormControlProps} className={clsx(classes.formControl, FormControlProps.className)}>
+    <div>
+      <div>
         <FieldArray key={rest.input.name} name={rest.input.name} validate={arrayValidator}>
           {({ fields: { map, value = [], push, remove } }) => {
             const pushWrapper = () => {
@@ -162,33 +129,27 @@ const DynamicArray = ({ ...props }) => {
             };
 
             return (
-              <Grid container spacing={3}>
-                <Grid item xs={12} className={classes.header}>
-                  {label && (
-                    <Typography variant="h6" className={classes.label}>
-                      {label}
-                    </Typography>
-                  )}
-                  <IconButton color="primary" aria-label="undo" component="span" disabled={state.index === 0} onClick={undo}>
-                    <UndoIcon />
-                  </IconButton>
-                  <IconButton color="primary" aria-label="redo" component="span" disabled={state.index === state.history.length} onClick={redo}>
-                    <RedoIcon />
-                  </IconButton>
+              <div>
+                <div>
+                  {label && <h1>{label}</h1>}
+                  <Button disabled={state.index === 0} onClick={undo}>
+                    Undo
+                  </Button>
+                  <Button disabled={state.index === state.history.length} onClick={redo}>
+                    Redu
+                  </Button>
                   <Button color="primary" onClick={pushWrapper} disabled={value.length >= maxItems}>
                     {combinedButtonLabels.add}
                   </Button>
-                </Grid>
+                </div>
                 {description && (
-                  <Grid item xs={12}>
-                    <Typography variant="subtitle1">{description}</Typography>
-                  </Grid>
+                  <div item xs={12}>
+                    <h2>{description}</h2>
+                  </div>
                 )}
-                <Grid item xs={12}>
+                <div item xs={12}>
                   {value.length <= 0 ? (
-                    <Typography variant="body1" gutterBottom className={classes.centerText}>
-                      {noItemsMessage}
-                    </Typography>
+                    <p>{noItemsMessage}</p>
                   ) : (
                     map((name, index) => (
                       <ArrayItem
@@ -203,18 +164,18 @@ const DynamicArray = ({ ...props }) => {
                       />
                     ))
                   )}
-                </Grid>
+                </div>
                 {isError && (
-                  <Grid item xs={12}>
-                    <FormHelperText>{error}</FormHelperText>
-                  </Grid>
+                  <div item xs={12}>
+                    <p>{error}</p>
+                  </div>
                 )}
-              </Grid>
+              </div>
             );
           }}
         </FieldArray>
-      </FormControl>
-    </FormFieldGrid>
+      </div>
+    </div>
   );
 };
 
@@ -226,17 +187,13 @@ DynamicArray.propTypes = {
   minItems: PropTypes.number,
   maxItems: PropTypes.number,
   noItemsMessage: PropTypes.node,
-  FormControlProps: PropTypes.object,
-  FormFieldGridProps: PropTypes.object,
   buttonLabels: PropTypes.object
 };
 
 DynamicArray.defaultProps = {
   maxItems: Infinity,
   minItems: 0,
-  noItemsMessage: 'No items added',
-  FormControlProps: {},
-  FormFieldGridProps: {}
+  noItemsMessage: 'No items added'
 };
 
 export default DynamicArray;
