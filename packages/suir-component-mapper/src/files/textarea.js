9540ca46928641c5e6d75c08e6f723dfc5697269
Martin Maroši <marvusm.mmi@gmail.com>
2020-05-13 09:44:20 +0200

    fix(suir): Enable DDF props on textarea

--- packages/suir-component-mapper/src/files/textarea.js
+++ packages/suir-component-mapper/src/files/textarea.js
@@ -6,26 +6,22 @@ import FormFieldGrid from '../common/form-field-grid';
 import { validationError } from '../common/helpers';
 import { meta, input } from '@data-driven-forms/common/src/prop-types-templates';
 import { useFieldApi } from '@data-driven-forms/react-form-renderer';
+import FormField from '../common/form-field';
 
 const Textarea = (props) => {
-  const {
-    input,
-    isReadOnly,
-    isDisabled,
-    placeholder,
-    isRequired,
-    label,
-    helperText,
-    description,
-    validateOnMount,
-    meta,
-    inputProps,
-    ...rest
-  } = useFieldApi(props);
+  const { input, isReadOnly, isDisabled, placeholder, isRequired, helperText, validateOnMount, meta, ...rest } = useFieldApi(props);
   const invalid = validationError(meta, validateOnMount);
   return (
-    <FormFieldGrid>
-      <Form.TextArea {...input} error={invalid && { content: meta.error }} />
+    <FormFieldGrid helperText={helperText}>
+      <FormField
+        required={isRequired}
+        disabled={isDisabled}
+        readOnly={isReadOnly}
+        {...input}
+        error={invalid && { content: meta.error }}
+        control={Form.TextArea}
+        {...rest}
+      />
     </FormFieldGrid>
   );
 };
