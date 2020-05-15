0d1b03ada44d41895209e6bd5e062dcefbb3f5d7
Martin Maroši <marvusm.mmi@gmail.com>
2020-05-15 16:00:11 +0200

    Added favicon to docs

--- packages/react-renderer-demo/src/app/pages/_document.js
+++ packages/react-renderer-demo/src/app/pages/_document.js
@@ -10,6 +10,7 @@ class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <link rel="manifest" href="/manifest.json" />
+          <link rel="shortcut icon" href="/favicon.ico" />
           <link rel="apple-touch-icon" href="/192x192.png"></link>
           <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no" />
           {/* PWA primary color */}
