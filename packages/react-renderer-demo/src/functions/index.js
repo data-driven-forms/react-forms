203ba63fcbfe27024cab00cd434969a0bb11161c
Martin Maroši <marvusm.mmi@gmail.com>
2020-05-15 13:50:00 +0200

    Add service worker for docs offline mode.

--- packages/react-renderer-demo/src/functions/index.js
+++ packages/react-renderer-demo/src/functions/index.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const functions = require('firebase-functions');
 const next = require('next');
+const { join } = require('path');
+const { parse } = require('url');
 
 let dev = process.env.NODE_ENV !== 'production';
 let app = next({
@@ -11,5 +13,17 @@ let handle = app.getRequestHandler();
 
 exports.next = functions.https.onRequest((req, res) => {
   console.log('File: ' + req.originalUrl); // eslint-disable-line no-console
-  return app.prepare().then(() => handle(req, res));
+  return app.prepare().then(() => {
+    const parsedUrl = parse(req.url, true);
+    const { pathname } = parsedUrl;
+
+    // handle GET request to /service-worker.js
+    if (pathname === '/service-worker.js') {
+      const filePath = join(__dirname, '.next', pathname);
+
+      app.serveStatic(req, res, filePath);
+    } else {
+      handle(req, res, parsedUrl);
+    }
+  });
 });
