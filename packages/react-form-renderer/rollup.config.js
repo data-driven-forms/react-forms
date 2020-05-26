d59f2f03decdcdc13d48fa4f4e5aafad013738b7
Martin Maroši <marvusm.mmi@gmail.com>
2020-05-26 09:45:27 +0200

    feat(renderer): add TS config.

--- packages/react-form-renderer/rollup.config.js
+++ packages/react-form-renderer/rollup.config.js
@@ -4,11 +4,12 @@ import babel from 'rollup-plugin-babel';
 import replace from 'rollup-plugin-replace';
 import nodeGlobals from 'rollup-plugin-node-globals';
 import { terser } from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
 import glob from 'glob';
 import path from 'path';
 import sourcemaps from 'rollup-plugin-sourcemaps';
 
-const outputPaths = glob.sync(path.resolve(__dirname, './src/files/*.js'));
+const outputPaths = [...glob.sync(path.resolve(__dirname, './src/files/!(*.d.ts)')), ...glob.sync(path.resolve(__dirname, './src/files/*.js'))];
 
 const globals = {
   react: 'React',
@@ -42,6 +43,9 @@ const plugins = [
   terser({
     keep_classnames: true,
     keep_fnames: true
+  }),
+  typescript({
+    allowSyntheticDefaultImports: true
   }),
   sourcemaps()
 ];
