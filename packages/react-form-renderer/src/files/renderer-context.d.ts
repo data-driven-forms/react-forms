231ed86bd64bb0051b1e986f44ebe36aa5779818
Martin Maroši <marvusm.mmi@gmail.com>
2020-06-01 13:09:02 +0200

    fix(renderer): fixup typings requirements

--- packages/react-form-renderer/src/files/renderer-context.d.ts
+++ packages/react-form-renderer/src/files/renderer-context.d.ts
@@ -6,11 +6,11 @@ import ActionMapper from './action-mapper';
 import Field from './field';
 
 export interface FormOptions extends FormApi {
-  registerInputFile: (name: string) => void;
-  unRegisterInputFile: (name: string) => void;
-  onCancel: (values: object, ...args: any[]) => void;
-  onReset: () => void;
-  clearedValue: any;
+  registerInputFile?: (name: string) => void;
+  unRegisterInputFile?: (name: string) => void;
+  onCancel?: (values: object, ...args: any[]) => void;
+  onReset?: () => void;
+  clearedValue?: any;
   renderForm: (fields: Field[]) => ReactNode[];
 }
 
