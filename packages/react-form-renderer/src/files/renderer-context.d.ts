86f0998a325aa07af0af21a8f7aef604b9e7a46d
Martin Maroši <marvusm.mmi@gmail.com>
2020-06-01 17:08:05 +0200

    fix(renderer): fixed incorrect typings
    - added AnyObject type for extensible props
    - fixed component mapper expecting string instead of react component
    - fixed useFieldApi return type to accept other props that input and meta
    - fixed missing component type in form template
    - fixed SormSpy not being exported as component
    - added form template render props
    - fixed wrong return type of useFormApi hook
    - fixed field validator type schema definition

--- packages/react-form-renderer/src/files/renderer-context.d.ts
+++ packages/react-form-renderer/src/files/renderer-context.d.ts
@@ -1,15 +1,18 @@
 import { ReactNode } from 'react';
 import { FormApi } from 'final-form';
+import { FormRenderProps } from 'react-final-form';
 import ComponentMapper from './component-mapper';
 import ValidatorMapper from './validator-mapper';
 import ActionMapper from './action-mapper';
 import Field from './field';
+import { AnyObject } from './common';
 
 export interface FormOptions extends FormApi {
   registerInputFile?: (name: string) => void;
   unRegisterInputFile?: (name: string) => void;
   onCancel?: (values: object, ...args: any[]) => void;
   onReset?: () => void;
+  handleSubmit: () => Promise<AnyObject | undefined> | undefined;
   clearedValue?: any;
   renderForm: (fields: Field[]) => ReactNode[];
 }
