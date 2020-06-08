42361534198986dabb3d088faf364055a1bbd70a
Martin Maroši <marvusm.mmi@gmail.com>
2020-06-08 11:01:14 +0200

    fix(pf): added basic types sctructure to wizard

--- packages/pf4-component-mapper/src/files/wizard.d.ts
+++ packages/pf4-component-mapper/src/files/wizard.d.ts
@@ -1,11 +1,58 @@
 import { ReactNode } from "react";
-import { Field } from "@data-driven-forms/react-form-renderer";
+import { Field, AnyObject, FormOptions } from "@data-driven-forms/react-form-renderer";
 
 export interface WizardButtonLabels {
   submit?: ReactNode;
   cancel?: ReactNode;
   back?: ReactNode;
   next?: ReactNode;
+}
+
+export interface WizardNextStepFunctionArgument {
+  values?: AnyObject;
+  value?: any;
+}
+
+export interface WizardNextStepFunction {
+  (formState: WizardNextStepFunctionArgument): string;
+}
+
+export interface WizardStepMapper {
+  [key: string]: string | number;
+}
+
+export interface WizardNextStepMapper {
+  when: string;
+  stepMapper: WizardStepMapper;
+}
+
+export type WizardNextStep = string | WizardNextStepMapper | WizardNextStepFunction;
+
+export interface WizardButtonsProps {
+  ConditionalNext: React.ComponentType;
+  SubmitButton: React.ComponentType;
+  SimpleNext: React.ComponentType;
+  formOptions: FormOptions;
+  disableBack?: boolean;
+  handlePrev: any;
+  nextStep?: string | number;
+  FieldProvider?: React.ComponentType;
+  handleNext: any;
+  buttonsClassname?: string;
+  buttonLabels: AnyObject;
+  renderNextButton: any;
+}
+
+export interface WizardField {
+  name: string | number;
+  fields: Field[];
+  nextStep?: WizardNextStep;
+  substepOf?: string | number;
+  title?: ReactNode;
+  showTitle?: boolean;
+  customTitle?: ReactNode;
+  disableForwardJumping?: boolean;
+  buttons?: ReactNode | React.ComponentType<WizardButtonsProps>;
 }
 
 export interface WizardProps {
@@ -20,7 +67,7 @@ export interface WizardProps {
   isDynamic?: boolean;
   showTitles?: boolean;
   crossroads?: string[];
-  fields: Field[];
+  fields: WizardField[];
 }
 
 declare const Wizard: React.ComponentType<WizardProps>;
