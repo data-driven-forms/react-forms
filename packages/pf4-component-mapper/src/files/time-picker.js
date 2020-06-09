3af14908d97a4147c5bc67886310f33760aa1d1d
Richard Vsiansky <r.vsia@seznam.cz>
2020-06-09 17:55:26 +0200

    feat(pf4): add validated to components supporting it

--- packages/pf4-component-mapper/src/files/time-picker.js
+++ packages/pf4-component-mapper/src/files/time-picker.js
@@ -3,6 +3,7 @@ import FormGroup from '../common/form-group';
 import { TextInput } from '@patternfly/react-core';
 import { useFieldApi } from '@data-driven-forms/react-form-renderer';
 import PropTypes from 'prop-types';
+import showError from '../common/show-error';
 
 const TimePicker = (props) => {
   const { label, isRequired, helperText, meta, description, hideLabel, input, isReadOnly, isDisabled, id, ...rest } = useFieldApi(props);
@@ -16,7 +17,7 @@ const TimePicker = (props) => {
       hideLabel={hideLabel}
       id={id || input.name}
     >
-      <TextInput {...input} {...rest} type="time" id={id || input.name} isReadOnly={isReadOnly} isDisabled={isDisabled} />
+      <TextInput {...input} {...rest} type="time" id={id || input.name} isReadOnly={isReadOnly} isDisabled={isDisabled} {...showError(meta)} />
     </FormGroup>
   );
 };
