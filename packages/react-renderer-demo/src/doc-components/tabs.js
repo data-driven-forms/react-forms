607df764cc2c58d418017ffffe61cf5c2f77359e
Richard Vsiansky <r.vsia@seznam.cz>
2020-06-10 15:43:47 +0200

    Update documenation to latest PF4

--- packages/react-renderer-demo/src/doc-components/tabs.js
+++ packages/react-renderer-demo/src/doc-components/tabs.js
@@ -2,6 +2,8 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import MuiTabs from './examples-texts/mui/mui-tabs.md';
 import SuirTabs from './examples-texts/suir/suir-tabs.md';
+import Pf4Tabs from './examples-texts/pf4/pf4-tabs.md';
+
 import GenericMuiComponent from '../helpers/generic-mui-component';
 
 const Tabs = ({ activeMapper }) => {
@@ -11,6 +13,10 @@ const Tabs = ({ activeMapper }) => {
 
   if (activeMapper === 'suir') {
     return <SuirTabs />;
+  }
+
+  if (activeMapper === 'pf4') {
+    return <Pf4Tabs />;
   }
 
   return <GenericMuiComponent activeMapper={activeMapper} component="tabs" />;
