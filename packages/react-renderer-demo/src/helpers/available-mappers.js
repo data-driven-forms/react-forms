cf62f49c3fc503ccd7a347e87f268aa4cf2d6edc
Arun <arunthulasiraman@gmail.com>
2020-07-10 20:42:45 +0530

    Add stackblitz templates of ant components

--- packages/react-renderer-demo/src/helpers/available-mappers.js
+++ packages/react-renderer-demo/src/helpers/available-mappers.js
@@ -3,7 +3,8 @@ const avalableMappers = [
   { title: 'PF4', mapper: 'pf4' },
   { title: 'PF3', mapper: 'pf3' },
   { title: 'BJS', mapper: 'blueprint' },
-  { title: 'SUIR', mapper: 'suir' }
+  { title: 'SUIR', mapper: 'suir' },
+  { title: 'ANT', mapper: 'ant' }
 ];
 
 export default avalableMappers;
