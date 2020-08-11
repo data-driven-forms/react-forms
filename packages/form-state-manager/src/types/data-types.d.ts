956a6cfc65e7491f8ee986e90f69f40d42ffd212
Richard Vsiansky <r.vsia@seznam.cz>
2020-08-11 11:17:25 +0200

    feat(state): add DataType to useSubscription

---
+++ packages/form-state-manager/src/types/data-types.d.ts
@@ -0,0 +1,13 @@
+export type DataType = 'integer' | 'float' | 'number' | 'boolean' | 'string';
+
+interface dataTypes {
+  INTEGER: 'integer';
+  FLOAT: 'float';
+  NUMBER: 'number';
+  BOOLEAN: 'boolean';
+  STRING: 'string';
+}
+
+declare const dataTypes: dataTypes;
+
+export default dataTypes;
