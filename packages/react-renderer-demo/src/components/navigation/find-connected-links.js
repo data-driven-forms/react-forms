8b874e545c1f2f69dd26cb701fd9756a98e33276
Richard Vsiansky <r.vsia@seznam.cz>
2020-08-31 09:56:48 +0200

    Fix connected links

--- packages/react-renderer-demo/src/components/navigation/find-connected-links.js
+++ packages/react-renderer-demo/src/components/navigation/find-connected-links.js
@@ -1,5 +1,6 @@
 export const query = /\?.*/;
 
-const findConnectedLinks = (pathname, navSchema) => navSchema.find(({ link }) => pathname.replace(/^\//, '').replace(query, '') === link);
+const findConnectedLinks = (pathname, navSchema) =>
+  navSchema.find(({ link }) => pathname.replace(/^\//, '').replace(query, '') === link.replace(query, ''));
 
 export default findConnectedLinks;
