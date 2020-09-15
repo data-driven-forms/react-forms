2c35dd0e5311b1055e8a89cc901d10aa7dc8f72e
Richard Vsiansky <r.vsia@seznam.cz>
2020-09-15 12:48:42 +0200

    feat(carbon): add field array component

--- packages/carbon-component-mapper/src/files/field-array.js
+++ packages/carbon-component-mapper/src/files/field-array.js
@@ -2,7 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useFieldApi, useFormApi, FieldArray as FieldArrayFF } from '@data-driven-forms/react-form-renderer';
 
-const ArrayItem = ({ remove, fields, name }) => {
+import { Button, FormGroup } from 'carbon-components-react';
+import { AddAlt32, Subtract32 } from '@carbon/icons-react';
+
+import './field-array.scss';
+
+import prepareProps from '../common/prepare-props';
+
+const ArrayItem = ({ remove, fields, name, removeText, buttonDisabled }) => {
   const formOptions = useFormApi();
 
   const editedFields = fields.map((field) => ({
@@ -13,7 +20,16 @@ const ArrayItem = ({ remove, fields, name }) => {
   return (
     <div>
       {formOptions.renderForm(editedFields, formOptions)}
-      <button onClick={remove}>Remove</button>
+      <Button
+        disabled={buttonDisabled}
+        renderIcon={Subtract32}
+        id={`remove-${name}`}
+        kind="danger"
+        onClick={remove}
+        className={'ddorg__carbon-field-array-remove'}
+      >
+        {removeText}
+      </Button>
     </div>
   );
 };
@@ -21,24 +37,74 @@ const ArrayItem = ({ remove, fields, name }) => {
 ArrayItem.propTypes = {
   remove: PropTypes.func,
   fields: PropTypes.array,
-  name: PropTypes.string
+  name: PropTypes.string,
+  removeText: PropTypes.node,
+  buttonDisabled: PropTypes.bool
 };
 
 const FieldArray = (props) => {
-  const { itemDefault, fields, input, arrayValidator } = useFieldApi(props);
+  const { itemDefault, maxItems, minItems, fields, input, arrayValidator, labelText, buttonLabels, noItemsMessage, meta } = useFieldApi(
+    prepareProps(props)
+  );
+
+  const buttonLabelsFinal = {
+    add: 'Add',
+    remove: 'Remove',
+    ...buttonLabels
+  };
+
+  const invalid = meta.touched && meta.error && !Array.isArray(meta.error);
 
   return (
-    <FieldArrayFF name={input.name} validate={arrayValidator}>
-      {(fieldArrayProps) => (
-        <div>
-          {fieldArrayProps.fields.map((name, index) => (
-            <ArrayItem key={index} remove={() => fieldArrayProps.fields.remove(index)} name={name} fields={fields} />
-          ))}
-          <button onClick={() => fieldArrayProps.fields.push(itemDefault)}>Add</button>
-        </div>
-      )}
-    </FieldArrayFF>
+    <FormGroup
+      legendText={labelText}
+      invalid={Boolean(invalid)}
+      message={Boolean(invalid)}
+      messageText={invalid || ''}
+      className={'ddorg__carbon-field-array-form-group'}
+    >
+      <FieldArrayFF name={input.name} validate={arrayValidator}>
+        {(fieldArrayProps) => (
+          <div>
+            {fieldArrayProps.fields.length === 0 && noItemsMessage}
+            {fieldArrayProps.fields.map((name, index) => (
+              <ArrayItem
+                removeText={buttonLabelsFinal.remove}
+                key={index}
+                remove={() => fieldArrayProps.fields.remove(index)}
+                name={name}
+                fields={fields}
+                buttonDisabled={minItems >= fieldArrayProps.fields.length}
+              />
+            ))}
+            <div className={'ddorg__carbon-field-array-add-container'}>
+              <Button
+                disabled={fieldArrayProps.fields.length >= maxItems}
+                renderIcon={AddAlt32}
+                id={`add-${input.name}`}
+                onClick={() => fieldArrayProps.fields.push(itemDefault)}
+                className={'ddorg__carbon-field-array-add'}
+              >
+                {buttonLabelsFinal.add}
+              </Button>
+            </div>
+          </div>
+        )}
+      </FieldArrayFF>
+    </FormGroup>
   );
+};
+
+FieldArray.propTypes = {
+  noItemsMessage: PropTypes.node,
+  maxItems: PropTypes.number,
+  minItems: PropTypes.number
+};
+
+FieldArray.defaultProps = {
+  noItemsMessage: 'No items',
+  maxItems: Infinity,
+  minItems: 0
 };
 
 export default FieldArray;
