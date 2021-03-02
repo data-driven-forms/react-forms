c9e0fada092c42e0a4754ca156fbbb3ab33e90cb
Martin Marosi <marvusm.mmi@gmail.com>
2021-03-02 12:26:39 +0100

    Duplicate docs babel config as per eslint request.

---
+++ packages/react-renderer-demo/babel.config.js
@@ -0,0 +1,16 @@
+const muiTransformPlugin = [
+  'transform-imports',
+  {
+    '@material-ui/core': {
+      transform: (importName) => `@material-ui/core/${importName}`,
+      preventFullImport: false,
+      skipDefaultConversion: false
+    }
+  },
+  'MUI-CJS'
+];
+
+module.exports = {
+  presets: [['next/babel']],
+  plugins: [muiTransformPlugin]
+};
