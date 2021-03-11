58e5c5b41d93a8f33a934d890f22fabec6ade6c3
rvsia <r.vsia@seznam.cz>
2021-03-11 09:40:18 +0100

    fix(renderer): fix numbers conversions

--- packages/react-form-renderer/src/tests/form-renderer/data-types.test.js
+++ packages/react-form-renderer/src/tests/form-renderer/data-types.test.js
@@ -4,6 +4,8 @@ import FormRenderer from '../../form-renderer';
 import FormTemplate from '../../../../../__mocks__/mock-form-template';
 import componentTypes from '../../component-types';
 import useFieldApi from '../../use-field-api';
+import convertType from '../../use-field-api/convert-type';
+import dataTypes from '../../data-types';
 
 const DataTypeInput = (props) => {
   const { input, type, label } = useFieldApi(props);
@@ -68,5 +70,29 @@ describe('data types', () => {
       expect.anything(),
       expect.anything()
     );
+  });
+
+  describe('converTypes', () => {
+    describe('integer', () => {
+      it('converts empty string', () => {
+        expect(convertType(dataTypes.INTEGER, '')).toEqual('');
+      });
+
+      it('converts zero', () => {
+        expect(convertType(dataTypes.INTEGER, '0')).toEqual(0);
+      });
+
+      it('converts an integer', () => {
+        expect(convertType(dataTypes.INTEGER, '12132')).toEqual(12132);
+      });
+
+      it('converts a string', () => {
+        expect(convertType(dataTypes.INTEGER, 'abcd')).toEqual('abcd');
+      });
+
+      it('converts a negative integer', () => {
+        expect(convertType(dataTypes.INTEGER, '-12132')).toEqual(-12132);
+      });
+    });
   });
 });
