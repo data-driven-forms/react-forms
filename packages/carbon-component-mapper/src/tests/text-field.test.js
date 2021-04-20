aa84e9db1098e471936ed175b74dee1b8f493407
rvsia <r.vsia@seznam.cz>
2021-04-20 16:43:03 +0200

    Update paths in text-field test file

--- packages/carbon-component-mapper/src/tests/text-field.test.js
+++ packages/carbon-component-mapper/src/tests/text-field.test.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { mount } from 'enzyme';
 import { act } from 'react-dom/test-utils';
 
-import { FormRenderer, componentTypes } from '@data-driven-forms/react-form-renderer';
+import FormRenderer, { componentTypes } from '@data-driven-forms/react-form-renderer';
 
-import FormTemplate from '../form-template';
-import componentMapper from '../component-mapper';
+import FormTemplate from '../files/form-template';
+import componentMapper from '../files/component-mapper';
 import { TextInput, NumberInput } from 'carbon-components-react';
 
 describe('<TextInput and NumberInput />', () => {
