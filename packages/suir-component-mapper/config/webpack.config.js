0dcd688bf0368544e4b131cea8e86c9a1163ef46
rvsia <r.vsia@seznam.cz>
2021-09-16 14:26:56 +0200

    Use common webpack 5 config

--- packages/suir-component-mapper/config/webpack.config.js
+++ packages/suir-component-mapper/config/webpack.config.js
@@ -1,38 +1,6 @@
-const TerserPlugin = require('terser-webpack-plugin');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const resolve = require('path').resolve;
-const merge = require('webpack-merge');
-
-const commonConfig = {
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader'
-        }
-      },
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader',
-        },
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'resolve-url-loader']
-      },
-      {
-        test: /\.(png|jpg|gif|svg|woff|ttf|eot)/,
-        loader: 'url-loader'
-      }
-    ]
-  }
-};
+const webpack = require('webpack');
 
 const htmlPlugin = new HtmlWebPackPlugin({
   template: './demo/index.html',
@@ -41,54 +9,35 @@ const htmlPlugin = new HtmlWebPackPlugin({
 
 const devConfig = {
   mode: 'development',
-  entry: './demo/index.js',
+  entry: { app: resolve('./demo/index.js') },
   output: {
     path: resolve('../dist'),
     filename: '[name].[hash].js'
   },
-  devtool: 'source-map',
-  plugins: [htmlPlugin]
-};
-
-const externals = [
-  {
-    react: {
-      root: 'React',
-      commonjs2: 'react',
-      commonjs: ['react'],
-      amd: 'react'
-    }
-  }
-];
-
-const prodConfig = {
-  mode: 'production',
-  entry: './src/index.js',
-  output: {
-    path: resolve('./dist'),
-    library: '[name]',
-    libraryTarget: 'umd',
-    filename: 'index.js'
-  },
-  optimization: {
-    minimizer: [
-      new TerserPlugin({
-        cache: true,
-        parallel: true,
-        terserOptions: {
-          keep_classnames: true,
-          keep_fnames: true
-        }
-      })
+  devtool: 'eval-source-map',
+  plugins: [
+    htmlPlugin,
+    new webpack.ProvidePlugin({
+      process: 'process/browser'
+    })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/i,
+        use: ['babel-loader']
+      },
+      {
+        test: /\.(sa|sc|c)ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: /\.(png|jpg|gif|svg|woff|ttf|eot)/,
+        type: 'asset/resource'
+      }
     ]
-  },
-  externals
+  }
 };
 
-module.exports = prodConfig;
-
-module.exports = (env) =>
-  ({
-    dev: merge(commonConfig, devConfig),
-    prod: merge(commonConfig, prodConfig)
-  }[env]);
+module.exports = devConfig;
