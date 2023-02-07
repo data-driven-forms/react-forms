6b7297a16343f3ef96a3e30af530bb7e14c6e6a0
Richard Všianský <richard.vsiansky@sap.com>
2023-02-07 12:31:58 +0100

    Update select

--- packages/ui5-component-mapper/src/select/select.js
+++ packages/ui5-component-mapper/src/select/select.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { useFieldApi } from '@data-driven-forms/react-form-renderer';
+import useSelect from '@data-driven-forms/common/use-select';
 
 import { Select as UI5Select, Option } from '@ui5/webcomponents-react';
 
@@ -9,20 +10,55 @@ import convertProps from '../convert-props';
 import validationError from '../validation-error';
 
 const Select = (props) => {
-  const { input, meta, label, validateOnMount, options, ...rest } = useFieldApi(convertProps(props));
+  const {
+    input,
+    meta,
+    label,
+    validateOnMount,
+    options,
+    loadOptions,
+    optionsTransformer,
+    simpleValue,
+    isMulti,
+    pluckSingleValue,
+    noValueUpdates,
+    isSearchable,
+    loadOptionsChangeCounter,
+    ...rest
+  } = useFieldApi(convertProps(props));
+
+  const {
+    value: selectValue,
+    onChange,
+    isFetching,
+    onInputChange,
+    state,
+  } = useSelect({
+    loadOptions,
+    optionsTransformer,
+    options,
+    noValueUpdates,
+    onChange: input.onChange,
+    value: input.value,
+    loadOptionsChangeCounter,
+    isSearchable,
+    pluckSingleValue,
+    isMulti,
+    simpleValue: false,
+  });
 
   return (
     <FormGroup label={label}>
       <UI5Select
         {...input}
-        onChange={(event) => input.onChange(event.detail.selectedOption.dataset.id)}
+        onChange={(event) => onChange(event.detail.selectedOption.dataset.id)}
         {...rest}
         {...validationError(meta, validateOnMount)}
       >
-        {options &&
-          options.map((option) => (
-            <Option key={option.value} data-id={option.value}>
-              {option.label}
+        {state.options &&
+          state.options.map(({ label, value, ...option }) => (
+            <Option key={value} data-id={value} {...option} selected={value === selectValue}>
+              {label}
             </Option>
           ))}
       </UI5Select>
