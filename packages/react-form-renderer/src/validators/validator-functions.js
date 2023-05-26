a69abb602c3071fd4efeafeb384358481fe3619c
Jspannu919 <35170151+jspannu919@users.noreply.github.com>
2023-05-26 12:58:39 +0530

    Added strict equality check

--- packages/react-form-renderer/src/validators/validator-functions.js
+++ packages/react-form-renderer/src/validators/validator-functions.js
@@ -90,7 +90,7 @@ export const numericality = memoize(
     lessOrEqual = selectNum(lessOrEqual, lessThanOrEqualTo);
 
     return prepare((value) => {
-      if (value == null) {
+      if (value === null || value === undefined) {
         return;
       }
 
