458030af3ea74ca3b9438db60da1ae304d78ae0a
Ahmed Mustafa <59970699+AhmedMustafa2201@users.noreply.github.com>
2023-07-05 16:04:16 +0000

    adding more logs to collect preview link

--- packages/react-renderer-demo/src/helpers/url-formatter.js
+++ packages/react-renderer-demo/src/helpers/url-formatter.js
@@ -1,16 +1,22 @@
 export const urlFormatter = (initPath) => {
-  const nextEnv = process.env.NEXT_PUBLIC_VERCEL_ENV;
-  const nextUrl = process.env.NEXT_PUBLIC_VERCEL_URL;
-  const nextBranchUrl = process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL;
+  const env = process.env.NEXT_PUBLIC_VERCEL_ENV;
+  const url = process.env.NEXT_PUBLIC_VERCEL_URL;
+  const branchUrl = process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL;
 
-  const env = process.env.VERCEL_ENV;
-  const url = process.env.VERCEL_URL;
-  const branchUrl = process.env.VERCEL_BRANCH_URL;
+  const {
+    NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG,
+    NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF,
+    NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA,
+    NEXT_PUBLIC_VERCEL_GIT_PREVIOUS_SHA,
+    NEXT_PUBLIC_VERCEL_GIT_COMMIT_AUTHOR_LOGIN,
+    NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER,
+  } = process.env;
+  console.log(`NEXT_PUBLIC_VERCEL_ENV= ${env}, NEXT_PUBLIC_VERCEL_URL= ${url}, NEXT_PUBLIC_VERCEL_BRANCH_URL= ${branchUrl}`);
+  console.log(
+    `VERCEL_GIT_REPO_SLUG= ${NEXT_PUBLIC_VERCEL_GIT_REPO_SLUG}, VERCEL_GIT_COMMIT_REF= ${NEXT_PUBLIC_VERCEL_GIT_COMMIT_REF}, VERCEL_GIT_COMMIT_SHA= ${NEXT_PUBLIC_VERCEL_GIT_COMMIT_SHA}, VERCEL_GIT_PREVIOUS_SHA= ${NEXT_PUBLIC_VERCEL_GIT_PREVIOUS_SHA}, VERCEL_GIT_COMMIT_AUTHOR_LOGIN= ${NEXT_PUBLIC_VERCEL_GIT_COMMIT_AUTHOR_LOGIN}, VERCEL_GIT_REPO_OWNER= ${NEXT_PUBLIC_VERCEL_GIT_REPO_OWNER}`
+  );
 
-  console.log(`NEXT_PUBLIC_VERCEL_ENV= ${nextEnv}, NEXT_PUBLIC_VERCEL_URL= ${nextUrl}, NEXT_PUBLIC_VERCEL_BRANCH_URL= ${nextBranchUrl}`);
-  console.log(`VERCEL_ENV= ${env}, VERCEL_URL= ${url}, VERCEL_BRANCH_URL= ${branchUrl}`);
-
-  if (env === 'preview' || nextEnv === 'preview') {
+  if (env === 'preview') {
     return `https://${branchUrl + initPath}`;
   }
 
